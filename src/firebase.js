f24ad248c7c73c1b6a942c67ed5a34f87c787629
Shubham <shubhamsikarwar43@gmail.com>
2021-07-19 16:55:04 +0530

    Firebase file is added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABxUig2u2m9rgkWCOBMx2clon1hTpBZUk",
+    authDomain: "netflix-clone-e4b0a.firebaseapp.com",
+    projectId: "netflix-clone-e4b0a",
+    storageBucket: "netflix-clone-e4b0a.appspot.com",
+    messagingSenderId: "133540293170",
+    appId: "1:133540293170:web:a2b792ab239900b36b6a2b"
+  };
