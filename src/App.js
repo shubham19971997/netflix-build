bbe831610a222bd3180608013142f01197aee6c7
Shubham <shubhamsikarwar43@gmail.com>
2021-07-21 15:04:22 +0530

    Frontend Part Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
-import Banner from './Banner'
+import Banner from './Banner';
+import requests from './Requests';
+import Row from './Row';
 
 function App() {
   return (
     <div className="app">
       <HomeScreen/>
       <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow/>
     </div>
   );
 }
--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Banner.css';
 import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
 
@@ -8,9 +9,18 @@ function Banner() {
 
     useEffect(()=>{
         async function fetchData(){
-            const request = await axios
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()*request.data.results.length-1)
+                ]
+            );
+            return request;
         }
-    },[])
+        fetchData();
+    },[]);
+
+    console.log(movie)
 
     function truncate(string,n){
         return string?.length>n?string.substr(0,n-1)+'...':string
@@ -19,16 +29,16 @@ function Banner() {
     return (
         <header className="banner" style={{
             backgroundSize:"cover",
-            backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center"
         }}> 
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Title</h1>
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
-                <h1 className="banner__description">{truncate(`This is a test description`,150)}</h1>
+                <h1 className="banner__description">{truncate(movie?.overview,150)}</h1>
             </div>
             <div className="banner--fadeBottom"/>
         </header>
